6eb521388765d732b36ec444e4a84a6a4f91ca01
Andrew <arwest@uiowa.edu>
2018-12-07 20:27:57 -0600

    created the interpreter and tested and passes all tests

--- Project/PlcInterp.fs
+++ Project/PlcInterp.fs
@@ -4,102 +4,84 @@ open Absyn
 open Environ
 
 let rec eval (e : expr) (env : plcVal env) : plcVal = 
-    // turn everything into plcVal return (the return values)
+    match e with 
+    | ConI i -> IntV i
     
-    // match e with 
-    // | ConI _ -> IntT
+    | ConB b -> BooV b
     
-    // | ConB _ -> BooT
+    | EList _ -> LisV []
     
-    // | EList t -> t
+    | Var v -> lookup env v
     
-    // | Var v -> lookup env v
+    | Let (x, e1, e2) -> let e1' = eval e1 env in
+                         let nenv = (x,e1') :: env in
+                           eval e2 nenv
     
-    // | Let (x, e1, e2) -> let t = teval e1 env in
-    //                      let nenv = (x,t) :: env in
-    //                        teval e2 nenv
+    | Letrec (f, x, _, e1, _, e2) -> 
+        let fclos = Clos (f, x, e1, env) in
+        let nenv = (f, fclos) :: env in
+            eval e2 nenv
     
-    // | Letrec (f, x, xt, e1, rt, e2) -> 
-    //     let ft = FunT (xt, rt) in
-    //     let e1env = (x, xt) :: (f, ft) :: env in
-    //     let e2env = (f,ft) :: env in
-    //     if teval e1 e1env = rt then
-    //         teval e2 e2env
-    //     else
-    //         failwith ("TypeChecker: Return type differs from expression result " + f)
+    | Prim1 (op, e) ->
+        let re = eval e env in
+        match (op, re) with
+        | ("!", BooV b)   -> if b then BooV false else BooV true
+        | ("-", IntV i)   -> IntV (-i)
+        | ("hd", LisV l)  -> l.Head
+        | ("tl", LisV l)  -> LisV l.Tail
+        | ("ise", LisV l) -> if List.isEmpty l then BooV true else BooV false
+        | ("print", _)    -> printfn "%s" (val2string re) ; TupV []
+        | _ -> failwith ("Interpreter: Invalid operation " + op + " on " + val2string re)
     
-    // | Prim1 (op, e) ->
-    //     let eType = teval e env in
-    //     match (op, eType) with
-    //     | ("!", BooT)     -> BooT
-    //     | ("-", IntT)     -> IntT
-    //     | ("hd", LisT t)  -> t
-    //     | ("tl", LisT _)  -> eType
-    //     | ("ise", LisT _) -> BooT
-    //     | ("print", _)    -> TupT []
-    //     | _ -> failwith ("TypeChecker: Operation " + op + " applied to invalid type")
+    | Prim2 (op, e1, e2) ->
+        let re1 = eval e1 env in
+        let re2 = eval e2 env in
+        match (op, re1, re2) with
+        | ("&&", BooV b1, BooV b2) -> if b1 && b2 then BooV true else BooV false 
+        | ("::", v , LisV l)       -> LisV (v :: l)
+        | ("+", IntV i1, IntV i2)  -> IntV (i1 + i2)
+        | ("*", IntV i1, IntV i2)  -> IntV (i1 * i2)
+        | ("-", IntV i1, IntV i2)  -> IntV (i1 - i2)
+        | ("/", IntV i1, IntV i2)  -> IntV (i1 / i2)
+        | ("<", IntV i1, IntV i2)  -> if i1 < i2 then BooV true else BooV false
+        | ("<=", IntV i1, IntV i2) -> if i1 <= i2 then BooV true else BooV false
+        | ("=", r1, r2)            -> if r1 = r2 then BooV true else BooV false
+        | ("!=", r1, r2)           -> if r1 = r2 then BooV false else BooV true
+        | (";", _, r)              -> r
+        | _ -> failwith ("Interpreter: Unknown operation " + op + " applied to type " + val2string re1 + " and type " + val2string re2)
     
-    // | Prim2 (op, e1, e2) ->
-    //     let te1 = teval e1 env in
-    //     let te2 = teval e2 env in
-    //     match (op, te1, te2) with
-    //     | ("&&", BooT, BooT) -> BooT
-    //     | ("::", t , LisT t') when t = t' -> LisT t
-    //     | ("+", IntT, IntT) -> IntT
-    //     | ("*", IntT, IntT) -> IntT
-    //     | ("-", IntT, IntT) -> IntT
-    //     | ("/", IntT, IntT) -> IntT
-    //     | ("<", IntT, IntT) -> BooT
-    //     | ("<=", IntT, IntT) -> BooT
-    //     | ("=", t1, t2) when t1 = t2 -> BooT
-    //     | ("!=", t1, t2) when t1 = t2 -> BooT
-    //     | (";", _, t) -> t
-    //     | _ -> failwith ("TypeChecker: Unknown operation " + op + " applied to type " + type2string te1 + " and type " + type2string te2)
-    
-    // | If (e1, e2, e3) -> 
-    //     let te1 = teval e1 env in
-    //     match te1 with
-    //     | BooT -> let te2 = teval e2 env in
-    //               let te3 = teval e3 env in
-    //               if te2 = te3 then
-    //                 te2
-    //               else
-    //                 failwith ("TypeCheker: Resulting expressions of 'if' must have same type; not " + type2string te2 + " != " + type2string te3)
-    //     | _ -> failwith ("TypeChecker: First expression of 'if' must be of type Bool")
+    | If (e1, e2, e3) -> 
+        let re1 = eval e1 env in
+        match re1 with
+        | BooV b when b -> eval e2 env
+        | BooV _ -> eval e3 env                   
+        | _ -> failwith ("Interpreter: First expression of 'if' didn't return a Bool")
 
-    // | Call (Var f, e) -> 
-    //     match lookup env f with
-    //     | FunT (xt, rt) -> 
-    //         let eType = teval e env
-    //         if  eType = xt then
-    //             rt
-    //         else 
-    //             failwith ("TypeChecker: Expected input type " + type2string xt + "; Observed input type " + type2string eType)
-    //     | _ -> failwith ("TypeChecker: Function " + f + " is undefined")
-    // | Call (Call (f,e'), e) -> 
-    //     let crt = teval (Call (f,e')) env in
-    //     match crt with
-    //     | FunT (xt, rt) ->
-    //         let eType = teval e env
-    //         if eType = xt then
-    //             rt
-    //         else failwith ("TypeChecker: Expected input type " + type2string xt + "; Observed input type " + type2string eType)
-    //     | _ -> failwith ("TypeChecker: Internal function call returned insufficient type")
+    | Call (Var f, e) -> 
+        let clos = lookup env f
+        match clos with
+        | Clos (f, x, e1, fenv) -> 
+            let valx = eval e env in
+            let nenv = (x,valx) :: (f, clos) :: fenv in
+                eval e1 nenv
+        | _ -> failwith ("Interpreter: Function " + f + " is undefined")
+    | Call (Call (f,e'), e) -> 
+        let cr = eval (Call (f,e')) env in
+        match cr with
+        | Clos (f, x, e1, fenv) ->
+            let valx = eval e env in
+            let nenv = (x,valx) :: fenv in
+                eval e1 nenv
+        | _ -> failwith ("Interpreter: Internal function call returned insufficient type")
 
-    // | Call _ -> failwith ("TypeChecker: Illegal call to a function")
+    | Call _ -> failwith ("Interpreter: Illegal call to a function")
 
-    // | Tuple eList -> TupT (List.map (fun e -> teval e env) eList)
+    | Tuple l -> TupV (List.map (fun e -> eval e env) l)
 
-    // | Sel (e, num) -> 
-    //     match teval e env with
-    //     | TupT t ->
-    //         if 0 < num && num <= List.length t then
-    //             List.item (num - 1) t
-    //         else
-    //             failwith ("TypeChecker: Index out of range for TupT " + list2string type2string "" t )
-    //     | _ -> failwith ("TypeChecker: Illegal operation #" + string num + "applied to non-tuple")
-    // | Anon (x, xt, e) -> 
-    //     let aenv = (x, xt) :: env in
-    //     let eType = teval e aenv in
-    //         FunT (xt, eType)
+    | Sel (e, num) -> 
+        match eval e env with
+        | TupV t -> List.item (num - 1) t                
+        | _ -> failwith ("Interpreter: Illegal operation #" + string num + "applied to non-tuple")
+        
+    | Anon (x, _, e) -> Clos ("", x, e, env)
 
